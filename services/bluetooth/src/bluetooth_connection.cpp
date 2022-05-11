f78cac61a7f596dd9026a4b7b9f91d22a332edcb
qian-nan-xu <xuqiannan@huawei.com>
2022-05-11 10:39:19 +0800

    Set emergency number
    Signed-off-by: qian-nan-xu <xuqiannan@huawei.com>

--- services/bluetooth/src/bluetooth_connection.cpp
+++ services/bluetooth/src/bluetooth_connection.cpp
@@ -39,8 +39,7 @@ void BluetoothConnection::Init()
 #ifdef ABILITY_BLUETOOTH_SUPPORT
     TELEPHONY_LOGI("BluetoothConnection init success!");
     Bluetooth::HandsFreeAudioGateway::GetProfile()->RegisterObserver(this);
-#endif  
-  
+#endif
 }
 
 bool BluetoothConnection::ConnectBtSco()
