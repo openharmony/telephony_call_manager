50db7ef236d4ac073fcb4128097d307ca55fb423
YOUR_NAME <liujiandong1@huawei.com>
2022-11-14 14:21:18 +0800

    0~fix: modify fuzz Signed-off-by: liujiandong <liujiandong1@huawei.com>1~

--- test/fuzztest/dialcall_fuzzer/dialcall_fuzzer.cpp
+++ test/fuzztest/dialcall_fuzzer/dialcall_fuzzer.cpp
@@ -14,42 +14,152 @@
  */
 
 #include "dialcall_fuzzer.h"
+
 #include <cstddef>
 #include <cstdint>
+#define private public
 #include "addcalltoken_fuzzer.h"
-#include "call_manager_client.h"
+#include "call_manager_service.h"
 #include "system_ability_definition.h"
 
 using namespace OHOS::Telephony;
 namespace OHOS {
+static bool g_isInited = false;
+constexpr int32_t SLOT_NUM = 2;
+constexpr int32_t ACTIVE_NUM = 2;
+constexpr int32_t VEDIO_STATE_NUM = 2;
+constexpr int32_t DIAL_SCENE_NUM = 3;
+constexpr int32_t DIAL_TYPE_NUM = 3;
+constexpr int32_t CALL_TYPE_NUM = 3;
+
+bool IsServiceInited()
+{
+    if (!g_isInited) {
+        DelayedSingleton<CallManagerService>::GetInstance()->OnStart();
+        if (DelayedSingleton<CallManagerService>::GetInstance()->GetServiceRunningState() ==
+            static_cast<int32_t>(CallManagerService::ServiceRunningState::STATE_RUNNING)) {
+            g_isInited = true;
+        }
+    }
+    return g_isInited;
+}
+
+bool HasCall(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return TELEPHONY_ERROR;
+    }
+    MessageParcel dataParcel;
+    dataParcel.WriteBuffer(data, size);
+    dataParcel.RewindRead(0);
+    MessageParcel reply;
+    return DelayedSingleton<CallManagerService>::GetInstance()->OnHasCall(dataParcel, reply);
+}
+
+int32_t GetCallState(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return TELEPHONY_ERROR;
+    }
+    MessageParcel dataParcel;
+    dataParcel.WriteBuffer(data, size);
+    dataParcel.RewindRead(0);
+    MessageParcel reply;
+    return DelayedSingleton<CallManagerService>::GetInstance()->OnGetCallState(dataParcel, reply);
+}
+
+int32_t GetCallWaiting(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return TELEPHONY_ERROR;
+    }
+    int32_t slotId = static_cast<int32_t>(size % SLOT_NUM);
+    MessageParcel dataParcel;
+    dataParcel.WriteInt32(slotId);
+    size_t dataSize = size - sizeof(int32_t);
+    dataParcel.WriteBuffer(data + sizeof(int32_t), dataSize);
+    dataParcel.RewindRead(0);
+    MessageParcel reply;
+    return DelayedSingleton<CallManagerService>::GetInstance()->OnGetCallWaiting(dataParcel, reply);
+}
+
+bool IsRinging(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return false;
+    }
+    MessageParcel dataParcel;
+    dataParcel.WriteBuffer(data, size);
+    dataParcel.RewindRead(0);
+    MessageParcel reply;
+    return DelayedSingleton<CallManagerService>::GetInstance()->OnIsRinging(dataParcel, reply);
+}
+
+bool IsInEmergencyCall(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return false;
+    }
+    MessageParcel dataParcel;
+    dataParcel.WriteBuffer(data, size);
+    dataParcel.RewindRead(0);
+    MessageParcel reply;
+    return DelayedSingleton<CallManagerService>::GetInstance()->OnIsInEmergencyCall(dataParcel, reply);
+}
+
+int32_t DialCall(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return TELEPHONY_ERROR;
+    }
+    std::string number(reinterpret_cast<const char *>(data), size);
+    auto numberU16 = Str8ToStr16(number);
+    int32_t accountId = static_cast<int32_t>(size % SLOT_NUM);
+    int32_t videoState = static_cast<int32_t>(size % VEDIO_STATE_NUM);
+    int32_t dialScene = static_cast<int32_t>(size % DIAL_SCENE_NUM);
+    int32_t dialType = static_cast<int32_t>(size % DIAL_TYPE_NUM);
+    int32_t callType = static_cast<int32_t>(size % CALL_TYPE_NUM);
+    MessageParcel dataParcel;
+    dataParcel.WriteString16(numberU16);
+    dataParcel.WriteInt32(accountId);
+    dataParcel.WriteInt32(videoState);
+    dataParcel.WriteInt32(dialScene);
+    dataParcel.WriteInt32(dialType);
+    dataParcel.WriteInt32(callType);
+    dataParcel.RewindRead(0);
+
+    MessageParcel reply;
+    return DelayedSingleton<CallManagerService>::GetInstance()->OnDialCall(dataParcel, reply);
+}
+
+int32_t SetCallWaiting(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return TELEPHONY_ERROR;
+    }
+    int32_t slotId = static_cast<int32_t>(size % SLOT_NUM);
+    int32_t activate = static_cast<int32_t>(size % ACTIVE_NUM);
+    MessageParcel dataParcel;
+    dataParcel.WriteInt32(slotId);
+    dataParcel.WriteBool(activate);
+    dataParcel.RewindRead(0);
+    MessageParcel reply;
+    return DelayedSingleton<CallManagerService>::GetInstance()->OnSetCallWaiting(dataParcel, reply);
+}
+
 void DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
 {
     if (data == nullptr || size == 0) {
         return;
     }
-    auto cmClient = DelayedSingleton<CallManagerClient>::GetInstance();
-    if (!cmClient) {
-        return;
-    }
 
-    cmClient->Init(TELEPHONY_CALL_MANAGER_SYS_ABILITY_ID);
-    std::string number(reinterpret_cast<const char *>(data), size);
-    OHOS::AppExecFwk::PacMap dialInfo;
-    int32_t accountId = static_cast<int32_t>(size % 2);
-    int32_t videoState = static_cast<int32_t>(size % 2);
-    int32_t dialScene = static_cast<int32_t>(size % 3);
-    int32_t dialType = static_cast<int32_t>(size % 3);
-
-    dialInfo.PutIntValue("accountId", accountId);
-    dialInfo.PutIntValue("videoState", videoState);
-    dialInfo.PutIntValue("dialScene", dialScene);
-    dialInfo.PutIntValue("dialType", dialType);
-    cmClient->HasCall();
-    cmClient->GetCallState();
-    cmClient->GetCallWaiting(accountId);
-    cmClient->IsRinging();
-    cmClient->IsInEmergencyCall();
-    cmClient->DialCall(Str8ToStr16(number), dialInfo);
+    HasCall(data, size);
+    GetCallState(data, size);
+    GetCallWaiting(data, size);
+    IsRinging(data, size);
+    IsInEmergencyCall(data, size);
+    DialCall(data, size);
+    SetCallWaiting(data, size);
 }
 }  // namespace OHOS
 
--- test/fuzztest/setcalltransferinfo_fuzzer/setcalltransferinfo_fuzzer.cpp
+++ test/fuzztest/setcalltransferinfo_fuzzer/setcalltransferinfo_fuzzer.cpp
@@ -17,34 +17,127 @@
 
 #include <cstddef>
 #include <cstdint>
-
+#define private public
 #include "addcalltoken_fuzzer.h"
-#include "call_manager_client.h"
+#include "call_manager_service.h"
+#include "call_manager_service_stub.h"
 #include "system_ability_definition.h"
 
 using namespace OHOS::Telephony;
 namespace OHOS {
+static bool g_isInited = false;
+constexpr int32_t SLOT_NUM = 2;
+constexpr int32_t TWO_INT_NUM = 2;
+
+bool IsServiceInited()
+{
+    if (!g_isInited) {
+        DelayedSingleton<CallManagerService>::GetInstance()->OnStart();
+        if (DelayedSingleton<CallManagerService>::GetInstance()->GetServiceRunningState() ==
+            static_cast<int32_t>(CallManagerService::ServiceRunningState::STATE_RUNNING)) {
+            g_isInited = true;
+        }
+    }
+    return g_isInited;
+}
+
+void IsImsSwitchEnabled(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t slotId = static_cast<int32_t>(size % SLOT_NUM);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteInt32(slotId);
+    size_t dataSize = size - sizeof(int32_t);
+    dataMessageParcel.WriteBuffer(data + sizeof(int32_t), dataSize);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CallManagerService>::GetInstance()->OnIsVoLteEnabled(dataMessageParcel, reply);
+}
+
+void GetImsConfig(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t slotId = static_cast<int32_t>(size % SLOT_NUM);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteInt32(slotId);
+    dataMessageParcel.WriteInt32(static_cast<ImsConfigItem>(size));
+    size_t dataSize = size - sizeof(int32_t) - sizeof(ImsConfigItem);
+    dataMessageParcel.WriteBuffer(data + sizeof(int32_t) + sizeof(ImsConfigItem), dataSize);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CallManagerService>::GetInstance()->OnGetImsConfig(dataMessageParcel, reply);
+}
+
+void GetImsFeatureValue(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t slotId = static_cast<int32_t>(size % SLOT_NUM);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteInt32(slotId);
+    dataMessageParcel.WriteInt32(static_cast<FeatureType>(size));
+    size_t dataSize = size - sizeof(int32_t) * TWO_INT_NUM;
+    dataMessageParcel.WriteBuffer(data + sizeof(int32_t) * TWO_INT_NUM, dataSize);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CallManagerService>::GetInstance()->OnGetImsFeatureValue(dataMessageParcel, reply);
+}
+
+void GetCallTransferInfo(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t slotId = static_cast<int32_t>(size % SLOT_NUM);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteInt32(slotId);
+    dataMessageParcel.WriteInt32(static_cast<int32_t>(size));
+    size_t dataSize = size - sizeof(int32_t) * TWO_INT_NUM;
+    dataMessageParcel.WriteBuffer(data + sizeof(int32_t) * TWO_INT_NUM, dataSize);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CallManagerService>::GetInstance()->OnGetTransferNumber(dataMessageParcel, reply);
+}
+
+void SetCallTransferInfo(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t slotId = static_cast<int32_t>(size % SLOT_NUM);
+    MessageParcel dataMessageParcel;
+    CallTransferInfo info;
+    (void)memcpy_s(info.transferNum, kMaxNumberLen, reinterpret_cast<const char *>(data), size);
+    info.settingType = CallTransferSettingType::CALL_TRANSFER_ENABLE;
+    info.type = CallTransferType::TRANSFER_TYPE_BUSY;
+    dataMessageParcel.WriteInt32(slotId);
+    dataMessageParcel.WriteRawData((const void *)&info, sizeof(CallTransferInfo));
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CallManagerService>::GetInstance()->OnSetTransferNumber(dataMessageParcel, reply);
+}
+
 void DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
 {
     if (data == nullptr || size == 0) {
         return;
     }
-    auto cmClient = DelayedSingleton<CallManagerClient>::GetInstance();
-    if (!cmClient) {
-        return;
-    }
 
-    cmClient->Init(TELEPHONY_CALL_MANAGER_SYS_ABILITY_ID);
-    int32_t slotId = static_cast<int32_t>(size % 3);
-    CallTransferInfo info;
-    (void)memcpy_s(info.transferNum, kMaxNumberLen, reinterpret_cast<const char *>(data), size);
-    info.settingType = CallTransferSettingType::CALL_TRANSFER_ENABLE;
-    info.type = CallTransferType::TRANSFER_TYPE_BUSY;
-    cmClient->IsImsSwitchEnabled(slotId);
-    cmClient->GetImsConfig(slotId, static_cast<ImsConfigItem>(size));
-    cmClient->GetImsFeatureValue(slotId, static_cast<FeatureType>(size));
-    cmClient->GetCallTransferInfo(slotId, static_cast<CallTransferType>(size));
-    cmClient->SetCallTransferInfo(slotId, info);
+    IsImsSwitchEnabled(data, size);
+    GetImsConfig(data, size);
+    GetImsFeatureValue(data, size);
+    GetCallTransferInfo(data, size);
+    SetCallTransferInfo(data, size);
 }
 } // namespace OHOS
 
