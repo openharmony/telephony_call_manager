ee38d7ad004ec03dbacd8ef2940452c3f21e9638
ohos-lsw <lishiwei6@huawei.com>
2024-03-22 19:37:42 +0800

    Add call failure dialog
    Signed-off-by: ohos-lsw <lishiwei6@huawei.com>

--- services/call/call_state_observer/src/call_data_base_helper.cpp
+++ services/call/call_state_observer/src/call_data_base_helper.cpp
@@ -329,7 +329,7 @@ int32_t CallDataBaseHelper::GetAirplaneMode(bool &isAirplaneModeOn)
         callDataHelper->Release();
         return TELEPHONY_ERR_DATABASE_READ_FAIL;
     }
-    int columnindex = 0；
+    int columnindex = 0;
     std::string value = "";
     result->GetColumnIndex(SETTING_VALUE, columnindex);
     result->GetString(columnindex, value);
