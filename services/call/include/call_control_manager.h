d8407d779bebefb64ea4887af228c419aa4a0397
AiQing <liaiqing1@huawei.com>
2024-03-23 14:15:07 +0800

    多路
    Signed-off-by: AiQing <liaiqing1@huawei.com>

--- services/call/include/call_control_manager.h
+++ services/call/include/call_control_manager.h
@@ -120,13 +120,13 @@ public:
     int32_t GetVoIPCallState(int32_t &state);
     int32_t AddCallLogAndNotification(sptr<CallBase> &callObjectPtr);
     int32_t HangUpVoipCall();
+    int32_t CarrierAndVoipConflictProcess(int32_t callId);
 
 private:
     void CallStateObserve();
     int32_t NumberLegalityCheck(std::string &number);
     int32_t BroadcastSubscriber();
     void PackageDialInformation(AppExecFwk::PacMap &extras, std::string accountNumber, bool isEcc);
-    int32_t CarrierAndVoipConflictProcess(int32_t callId);
 
 private:
     class SystemAbilityListener : public SystemAbilityStatusChangeStub {
