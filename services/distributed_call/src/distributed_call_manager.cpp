8fa3d5e7b2aded856b53f4eaeb985047c0ce3d65
wangwei30043812 <wangwei1033@huawei.com>
2024-04-23 19:38:56 +0800

    update
    Signed-off-by: wangwei30043812 <wangwei1033@huawei.com>

--- services/distributed_call/src/distributed_call_manager.cpp
+++ services/distributed_call/src/distributed_call_manager.cpp
@@ -369,8 +369,10 @@ void DistributedCallManager::SwitchOnDCallDevice(std::unique_ptr<AudioDevice> de
 void DistributedCallManager::SetCallState(bool isActive)
 {
     isCallActived_.store(isActive);
-    dCallDeviceSwitchedOn_.store(false);
-    ClearConnectedDCallDevice();
+    if (isActive == false) {
+        dCallDeviceSwitchedOn_.store(false);
+        ClearConnectedDCallDevice();
+    }
 }
 
 void DistributedCallManager::SwitchOnDCallDeviceAsync(const AudioDevice& device)
