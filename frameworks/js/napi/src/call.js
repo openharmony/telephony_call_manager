6e71040b4aefe6e2e661051ba61568bfdb8ebd60
n00810144 <niuliming@huawei.com>
2024-08-07 20:53:47 +0800

    后台拉起makecall
    Signed-off-by: n00810144 <niuliming@huawei.com>

--- frameworks/js/napi/src/call.js
+++ frameworks/js/napi/src/call.js
@@ -57,7 +57,7 @@ async function startAbility(args, context) {
     if (context) {
         await context.startAbility(config);
     } else {
-        call.makeCall;
+        call.makeCall(args[0]);
     }
 }
 
