4fcd09fff0d84339751305086480dc637642b8b4
n00810144 <niuliming@huawei.com>
2024-10-15 11:28:40 +0800

    狀態不一致
    Signed-off-by: n00810144 <niuliming@huawei.com>

--- services/call_report/src/call_state_report_proxy.cpp
+++ services/call_report/src/call_state_report_proxy.cpp
@@ -45,10 +45,6 @@ void CallStateReportProxy::CallStateUpdated(
         TELEPHONY_LOGI("voip call no need to report call state");
         return;
     }
-    if (nextState == TelCallState::CALL_STATUS_DISCONNECTING) {
-        TELEPHONY_LOGI("disconnecting call no need to report call state");
-        return;
-    }
     UpdateCallState(callObjectPtr, nextState);
     UpdateCallStateForSlotId(callObjectPtr, nextState);
 }
@@ -80,6 +76,10 @@ void CallStateReportProxy::UpdateCallState(sptr<CallBase> &callObjectPtr, TelCal
         foregroundCall->GetCallAttributeInfo(info);
     } else {
         info.callState = currentVoipCallState;
+    }
+    if (info.callState == TelCallState::CALL_STATUS_DISCONNECTING) {
+        TELEPHONY_LOGI("disconnecting call no need to report call state");
+        return;
     }
     if (info.callState == currentCallState_) {
         TELEPHONY_LOGI("foreground call state is not changed, currentCallState_:%{public}d!", currentCallState_);
@@ -130,6 +130,10 @@ void CallStateReportProxy::UpdateCallStateForSlotId(sptr<CallBase> &callObjectPt
     std::u16string accountNumber = Str8ToStr16(str);
     if (nextState == TelCallState::CALL_STATUS_ANSWERED) {
         info.callState = TelCallState::CALL_STATUS_ANSWERED;
+    }
+    if (info.callState == TelCallState::CALL_STATUS_DISCONNECTING) {
+        TELEPHONY_LOGI("disconnecting call no need to report call state");
+        return;
     }
     ReportCallStateForCallId(info.accountId, static_cast<int32_t>(info.callState), accountNumber);
 }
