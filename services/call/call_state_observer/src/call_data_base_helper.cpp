26881d71e18a98deb3a7d1a0fbf2b227cc7ddbea
wangwei30043812 <wangwei1033@huawei.com>
2024-11-22 19:45:26 +0800

    fix no calllog problem
    Signed-off-by: wangwei30043812 <wangwei1033@huawei.com>

--- services/call/call_state_observer/src/call_data_base_helper.cpp
+++ services/call/call_state_observer/src/call_data_base_helper.cpp
@@ -22,12 +22,16 @@
 #include "phonenumbers/phonenumber.pb.h"
 #include "phonenumberutil.h"
 #include "telephony_log_wrapper.h"
+#include "os_account_manager.h"
+#include "system_ability_definition.h"
 
 namespace OHOS {
 namespace Telephony {
 class AbsSharedResultSet;
 static constexpr const char *CALLLOG_URI = "datashare:///com.ohos.calllogability";
 static constexpr const char *CALL_SUBSECTION = "datashare:///com.ohos.calllogability/calls/calllog";
+static const std::string CALL_SUBSECTION_SILENCE =
+    "datasharePROXY://com.ohos.contactsdataability/calls/calllog?Proxy=true&user=";
 static constexpr const char *CONTACT_URI = "datashare:///com.ohos.contactsdataability";
 static constexpr const char *CALL_BLOCK = "datashare:///com.ohos.contactsdataability/contacts/contact_blocklist";
 static constexpr const char *CONTACT_DATA = "datashare:///com.ohos.contactsdataability/contacts/contact_data";
@@ -39,6 +43,8 @@ static constexpr const char *SETTINGS_AIRPLANE_MODE_URI =
     "datashare:///com.ohos.settingsdata/entry/settingsdata/SETTINGSDATA?Proxy=true&key=airplane_mode";
 static constexpr const char *SETTINGS_AIRPLANE_MODE = "settings.telephony.airplanemode";
 static constexpr const int32_t MAX_WAITIME_TIME = 10;
+static constexpr const int32_t RETRY_NUM = 3;
+static constexpr const int32_t RETRY_INTERVAL_10MS = 10 * 1000;
 constexpr int32_t E_OK = 0;
 
 CallDataRdbObserver::CallDataRdbObserver(std::vector<std::string> *phones)
@@ -126,12 +132,14 @@ void CallDataBaseHelper::UnRegisterObserver()
 
 bool CallDataBaseHelper::Insert(DataShare::DataShareValuesBucket &values)
 {
-    std::shared_ptr<DataShare::DataShareHelper> helper = CreateDataShareHelper(CALLLOG_URI);
-    if (helper == nullptr) {
-        TELEPHONY_LOGE("helper is nullptr!");
+    std::shared_ptr<DataShare::DataShareHelper> helper = nullptr;
+    std::string url;
+    bool result = GetHelperAndUrl(helper, url);
+    if (!result) {
+        TELEPHONY_LOGE("GetHelperAndUrl fail!");
         return false;
     }
-    Uri uri(CALL_SUBSECTION);
+    Uri uri(url);
     bool result = (helper->Insert(uri, values) > 0);
     helper->Release();
     return result;
@@ -199,15 +207,54 @@ bool CallDataBaseHelper::Query(ContactInfo &contactInfo, DataShare::DataSharePre
     return true;
 }
 
+bool CallDataBaseHelper::GetHelperAndUrl(std::shared_ptr<DataShare::DataShareHelper> &helper, std::string &url)
+{
+    int32_t userId = 0;
+    bool isUserUnlocked = false;
+    AccountSA::OsAccountManager::GetForegroundOsAccountLocalId(userId, isUserUnlocked);
+    TELEPHONY_LOGI("isUserUnlocked: %{public}d", isUserUnlocked);
+    if (!isUserUnlocked) {
+        usleep(RETRY_INTERVAL_10MS);
+        for (int retryNum = 0; retryNum < RETRY_NUM; retryNum++) {
+            sptr<ISystemAbilityManager> samgr = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+            if (samgr == nullptr) {
+                TELEPHONY_LOGE("Failed to get system ability magr.");
+            }
+            sptr<IRemoteObject> remoteObject = samgr->CheckSystemAbility(DISTRIBUTED_KV_DATA_SERVICE_ABILITY_ID);
+            if (remoteObject == nullptr) {
+                usleep(RETRY_INTERVAL_10MS);
+            } else {
+                break;
+            }
+            if (retryNum == RETRY_NUM -1) {
+                TELEPHONY_LOGE("max retry num.");
+                return false;
+            }
+        }
+        helper = createDataShareHelper(CALL_SUBSECTION_SILENCE + std::to_string(userId));
+        url = CALL_SUBSECTION_SILENCE + std::to_string(usrId);
+    } else {
+        helper = createDataShareHelper(CALLLOG_URI);
+        url = CALL_SUBSECTION;
+    }
+    if (helper == nullptr) {
+        TELEPHONY_LOGE("helper is nullptr.");
+        return false;
+    }
+    return true;
+}
+
 bool CallDataBaseHelper::QueryCallLog(
     std::map<std::string, int32_t> &phoneNumAndUnreadCountMap, DataShare::DataSharePredicates &predicates)
 {
-    std::shared_ptr<DataShare::DataShareHelper> helper = CreateDataShareHelper(CALLLOG_URI);
-    if (helper == nullptr) {
-        TELEPHONY_LOGE("helper is nullptr!");
+    std::shared_ptr<DataShare::DataShareHelper> helper = nullptr;
+    std::string url;
+    bool result = GetHelperAndUrl(helper, url);
+    if (!result) {
+        TELEPHONY_LOGE("GetHelperAndUrl fail!");
         return false;
     }
-    Uri uri(CALL_SUBSECTION);
+    Uri uri(url);
     std::vector<std::string> columns;
     columns.push_back(CALL_PHONE_NUMBER);
     auto resultSet = helper->Query(uri, predicates, columns);
@@ -240,12 +287,14 @@ bool CallDataBaseHelper::QueryCallLog(
 
 bool CallDataBaseHelper::QueryAndDeleteLimitedIds(DataShare::DataSharePredicates &predicates)
 {
-    std::shared_ptr<DataShare::DataShareHelper> helper = CreateDataShareHelper(CALLLOG_URI);
-    if (helper == nullptr) {
-        TELEPHONY_LOGE("helper is nullptr!");
+   std::shared_ptr<DataShare::DataShareHelper> helper = nullptr;
+    std::string url;
+    bool result = GetHelperAndUrl(helper, url);
+    if (!result) {
+        TELEPHONY_LOGE("GetHelperAndUrl fail!");
         return false;
     }
-    Uri uri(CALL_SUBSECTION);
+    Uri uri(url);
     std::vector<std::string> columns;
     columns.push_back(CALL_ID);
     auto resultSet = helper->Query(uri, predicates, columns);
