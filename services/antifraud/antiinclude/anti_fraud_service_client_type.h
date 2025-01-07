3a9e2b6976b3e7ef5e39c4523a019c0bddf581a4
niumingyu0 <niumingyu@huawei.com>
2025-01-07 14:06:28 +0000

    update services/antifraud/antiinclude/anti_fraud_service_client_type.h.
    Signed-off-by: niumingyu0 <niumingyu@huawei.com>

--- services/antifraud/antiinclude/anti_fraud_service_client_type.h
+++ services/antifraud/antiinclude/anti_fraud_service_client_type.h
@@ -1,1 +1,34 @@
-#include "antifraud_service.h"
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#ifndef ANTI_FRAUD_SERVICE_CLIENT_TYPE_H
+#define ANTI_FRAUD_SERVICE_CLIENT_TYPE_H
+ 
+#include <string>
+ 
+namespace OHOS::AntiFraudService {
+ 
+typedef struct {
+    int errCode;
+    bool result;
+    int modelVersion;
+    int fraudType;
+    std::string text;
+    float pvalue;
+} AntiFraudResult;
+ 
+}
+ 
+#endif
