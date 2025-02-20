494dab00f8ae83628dca92f5f3b88062591df490
fanrui0 <fanrui32@h-partners.com>
2025-02-20 17:30:52 +0800

    antifraud
    Signed-off-by: fanrui0 <fanrui32@h-partners.com>

--- services/call/src/call_status_manager.cpp
+++ services/call/src/call_status_manager.cpp
@@ -855,6 +855,12 @@ void CallStatusManager::TriggerAntiFraud(int32_t antiFraudState)
             break;
         }
     }
+    if (antiFraudState == static_cast<int32_t>(AntiFraudState::ANTIFRAUD_STATE_RISK) ||
+        antiFraudState == static_cast<int32_t>(AntiFraudState::ANTIFRAUD_STATE_FINISHED)) {
+        antiFraudSlotId_ = -1;
+        antiFraudIndex_ = -1;
+        TELEPHONY_LOGI("can begin a new antifraud");
+    }
 
     if (call == nullptr) {
         return;
@@ -870,12 +876,6 @@ void CallStatusManager::TriggerAntiFraud(int32_t antiFraudState)
         if (ret != TELEPHONY_SUCCESS) {
             TELEPHONY_LOGE("UpdateCallState failed, errCode:%{public}d", ret);
         }
-    }
-
-    if (antiFraudState == static_cast<int32_t>(AntiFraudState::ANTIFRAUD_STATE_RISK) ||
-        antiFraudState == static_cast<int32_t>(AntiFraudState::ANTIFRAUD_STATE_FINISHED)) {
-        antiFraudSlotId_ = -1;
-        antiFraudIndex_ = -1;
     }
 }
 
