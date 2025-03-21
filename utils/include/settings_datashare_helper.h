8b597542f123c748b929c9976ebdffc15a2c36b0
niumingyu0 <niumingyu@huawei.com>
2025-03-21 07:31:37 +0000

    update utils/include/settings_datashare_helper.h.
    Signed-off-by: niumingyu0 <niumingyu@huawei.com>

--- utils/include/settings_datashare_helper.h
+++ utils/include/settings_datashare_helper.h
@@ -36,7 +36,7 @@ public:
     static const std::string QUERY_MOTION_PICKUP_REDUCE_KEY;
 
     ErrCode Query(Uri& uri, const std::string& key, std::string& values);
-    int32_t Insert(Uri &uri, const std::string &key, std::string &value);
+    int32_t Insert(Uri &uri, const std::string &key, const std::string &value);
     int32_t Update(Uri &uri, const std::string &key, const std::string &value);
     bool RegisterToDataShare(const Uri &uri, const sptr<AAFwk::IDataAbilityObserver> &observer);
 private:
