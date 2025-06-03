3fb482de4e050537c8531e422a843e47483ce088
王鸿睿 <wanghongrui13@huawei.com>
2025-06-03 09:33:44 +0000

    通话未接通显示其他设备接听
    Signed-off-by: 王鸿睿 <wanghongrui13@huawei.com>

--- services/call/src/call_status_manager.cpp
+++ services/call/src/call_status_manager.cpp
@@ -2159,10 +2159,14 @@ void CallStatusManager::ClearPendingState(sptr<CallBase> &call)
 
 void CallStatusManager::RefreshCallDisconnectReason(const sptr<CallBase> &call, int32_t reason)
 {
+    std::string ownType = system::GetParameter("const.product.devicetype", "");
     switch (reason) {
         case static_cast<int32_t>(RilDisconnectedReason::DISCONNECTED_REASON_ANSWERED_ELSEWHER):
-            call->SetAnswerType(CallAnswerType::CALL_ANSWERED_ELSEWHER);
-            TELEPHONY_LOGI("call answered elsewhere");
+        if ((DelayedSingleton::GetInstance()->IsSinkRole()) ||
+            (ownType == "wearable")) {
+                call->SetAnswerType(CallAnswerType::CALL_ANSWERED_ELSEWHER);
+                TELEPHONY_LOGI("call answered elsewhere");
+            }
             break;
         case static_cast<int32_t>(RilDisconnectedReason::DISCONNECTED_REASON_NORMAL):
             {
