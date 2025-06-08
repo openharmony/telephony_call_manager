6f615d4c1578b03e77582aadc64dbaa7f8768194
fanrui0 <fanrui32@h-partners.com>
2025-06-08 22:31:00 +0800

    nearlink call
    Signed-off-by: fanrui0 <fanrui32@h-partners.com>

--- test/unittest/call_manager_gtest/include/call_manager_connect.h
+++ test/unittest/call_manager_gtest/include/call_manager_connect.h
@@ -374,6 +374,13 @@ public:
         return TELEPHONY_SUCCESS;
     }
 
+    int32_t OnPhoneStateChange(int32_t numActive, int32_t numHeld, int32_t callState,
+        const std::string &number)
+    {
+        TELEPHONY_LOGI("OnPhoneStateChange success!");
+        return TELEPHONY_SUCCESS;
+    }
+
 private:
     using CallAbilityCallbackFunc = int32_t (CallAbilityCallbackStub::*)(MessageParcel &data, MessageParcel &reply);
     int32_t OnUpdateCallStateInfoRequest(MessageParcel &data, MessageParcel &reply);
