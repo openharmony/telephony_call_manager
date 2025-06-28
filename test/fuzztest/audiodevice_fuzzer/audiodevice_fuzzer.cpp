0498991465c9064d3effbe954ea1509510b94e3a
邵夷恺 <shaoyikai6@huawei.com>
2025-06-28 07:07:13 +0000

    update test/fuzztest/audiodevice_fuzzer/audiodevice_fuzzer.cpp.
    Signed-off-by: 邵夷恺 <shaoyikai6@huawei.com>

--- test/fuzztest/audiodevice_fuzzer/audiodevice_fuzzer.cpp
+++ test/fuzztest/audiodevice_fuzzer/audiodevice_fuzzer.cpp
@@ -139,7 +139,7 @@ void AudioProxyFunc(const uint8_t *data, size_t size)
     audioProxy->GetRingerMode();
     audioProxy->GetVolume(audioVolumeType);
     audioProxy->SetVolume(audioVolumeType, volume);
-    audioProxy->SetVolume(audioVolumeType, volume,
+    audioProxy->SetVolumeWithDevice(audioVolumeType, volume,
         AudioStandard:DeviceType::DEVICE_TYPE_SPEAKER);
     audioProxy->SetMaxVolume(audioVolumeType);
     audioProxy->IsStreamActive(audioVolumeType);
