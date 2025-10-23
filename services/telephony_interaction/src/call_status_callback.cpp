bb72d2ee501bf2fd5f30d5c71abcacdd0b5a50a9
shao-yikai <shaoyikai6@huawei.com>
2025-10-23 10:52:07 +0800

    update: 更新文件 call_status_callback.cpp
    Signed-off-by: shao-yikai <shaoyikai6@huawei.com>

--- services/telephony_interaction/src/call_status_callback.cpp
+++ services/telephony_interaction/src/call_status_callback.cpp
@@ -52,10 +52,10 @@ int32_t CallStatusCallback::UpdateCallReportInfo(const CallReportInfo &info)
         detailInfo.voipCallInfo.isConferenceCall = info.voipCallInfo.isConferenceCall;
         detailInfo.voipCallInfo.isVoiceAnswerSupported = info.voipCallInfo.isVoiceAnswerSupported;
         detailInfo.voipCallInfo.isUserMuteRingToneSupported = info.voipCallInfo.isUserMuteRingToneSupported;
-        detailInfo.voipCallInfo.isCallControlByAudioDeviceSupported =
-            info.voipCallInfo.isCallControlByAudioDeviceSupported;
-        detailInfo.voipCallInfo.isVoipInitiatedDuringCellularCallSupported =
-            info.voipCallInfo.isVoipInitiatedDuringCellularCallSupported;
+        detailInfo.voipCallInfo.isCallControlByExternelAudioDeviceSupported =
+            info.voipCallInfo.isCallControlByExternelAudioDeviceSupported;
+        detailInfo.voipCallInfo.isDialingAllowedDuringSystemCall =
+            info.voipCallInfo.isDialingAllowedDuringSystemCall;
         detailInfo.voipCallInfo.hasMicPermission = info.voipCallInfo.hasMicPermission;
         detailInfo.voipCallInfo.isCapsuleSticky = info.voipCallInfo.isCapsuleSticky;
         detailInfo.voipCallInfo.uid = info.voipCallInfo.uid;
