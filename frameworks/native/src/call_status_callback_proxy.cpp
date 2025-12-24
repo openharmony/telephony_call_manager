8d4b4f823025f7a000ac2ef7566b7a82ff25c78a
lizhiqi_hw <lizhiqi14@huawei.com>
2025-12-24 22:01:26 +0800

    update: 更新文件 call_status_callback_proxy.cpp
    Signed-off-by: lizhiqi_hw <lizhiqi14@huawei.com>

--- frameworks/native/src/call_status_callback_proxy.cpp
+++ frameworks/native/src/call_status_callback_proxy.cpp
@@ -873,22 +873,25 @@ int32_t CallStatusCallbackProxy::PackUpdateCallsReportInfo(const CallsReportInfo
     return TELEPHONY_SUCCESS;
 }
 
-int32_t CallStatusCallbackProxy::HandleImsSuppSvcNotification(const ImsSuppSvcNotificationReportInfo &response)
+int32_t CallStatusCallbackProxy::HandleImsSuppExtChanged(const ImsSuppExtReportInfo &suppExtInfo)
 {
     MessageParcel dataParcel;
     MessageParcel replyParcel;
     MessageOption option;
-    TELEPHONY_LOGI("entry CallStatusCallbackProxy::HandleImsSuppSvcNotification");
     if (!dataParcel.WriteInterfaceToken(CallStatusCallbackProxy::GetDescriptor())) {
         return TELEPHONY_ERR_WRITE_DESCRIPTOR_TOKEN_FAIL;
     }
-    dataParcel.WriteInt32(response.code);
-    dataParcel.WriteInt32(response.callId);
+    dataParcel.WriteInt32(suppExtInfo.slotId);
+    dataParcel.WriteInt32(suppExtInfo.code);
+    dataParcel.WriteInt32(suppExtInfo.callIndex);
     if (Remote() == nullptr) {
         return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
     }
-    int32_t error = Remote()->SendRequest(IMS_SUPP_SVC_NOTIFICATION, dataParcel, replyParcel, option);
-    return 0;
+    int32_t error = Remote()->SendRequest(IMS_SUPP_EXT_CHANGE, dataParcel, replyParcel, option);
+    if (error != TELEPHONY_SUCCESS) {
+        return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
+    }
+    return replyParcel.ReadInt32();
 }
 
 #ifdef SUPPORT_RTT_CALL
