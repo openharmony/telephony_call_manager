690b23ba2e8bf54c5d6e4aa1b5109c456ef13bae
fanrui0 <fanrui32@h-partners.com>
2026-03-11 11:28:03 +0800

    modify ring tone play
    Signed-off-by: fanrui0 <fanrui32@h-partners.com>

--- services/audio/include/audio_control_manager.h
+++ services/audio/include/audio_control_manager.h
@@ -104,6 +104,7 @@ public:
 #endif
     bool ShouldPlaySoundTone();
     bool ShouldPlayRingback();
+    void SetCallAudioMode(int32_t mode, int32_t scenarios);
     void SetIncomingConflict(bool isConflict);
 
 private:
