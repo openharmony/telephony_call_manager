8e6baf21edc7a81d612c353ea50d4500e8ef6d2d
gcw_8avsHzpw <hanjinghui2@huawei.com>
2026-04-21 22:10:33 +0800

    修复多线程问题
    Signed-off-by: gcw_8avsHzpw <hanjinghui2@huawei.com>

--- services/call/include/call_control_manager.h
+++ services/call/include/call_control_manager.h
@@ -256,7 +256,7 @@ private:
     CallStateToApp VoIPCallState_ = CallStateToApp::CALL_STATE_IDLE;
     bool shouldDisconnect = true;
     bool ReduceRingToneVolume_ = false;
-    static bool alarmSeted;
+    static std::atomic<bool> alarmSeted;
     struct AnsweredCallQueue {
         bool hasCall = false;
         int32_t callId = 0;
