928e7c023463f889fa88d0b494e94cf89774826c
gcw_8avsHzpw <hanjinghui2@huawei.com>
2026-04-25 16:50:17 +0800

    test: 添加 IncomingFlashReminder 完整测试用例集
    Signed-off-by: gcw_8avsHzpw <hanjinghui2@huawei.com>

--- test/unittest/call_manager_zero_gtest/src/zero_branch_incoming_flash_reminder_test.cpp
+++ test/unittest/call_manager_zero_gtest/src/zero_branch_incoming_flash_reminder_test.cpp
@@ -143,5 +143,394 @@ HWTEST_F(IncomingFlashReminderTest, HandlerNullptr_Destructor, TestSize.Level0)
     EXPECT_TRUE(true);
 }
 
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_TorchNotSupported_001
+ * @tc.name     test torch not supported branch
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, TorchNotSupported, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(false);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_FALSE(reminder_->IsTorchReady());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_TorchAlreadyOn_001
+ * @tc.name     test torch already on branch
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, TorchAlreadyOn, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(1);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_FALSE(reminder_->IsTorchReady());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_TorchReadyOff_001
+ * @tc.name     test torch ready with mode off
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, TorchReadyOff, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_TRUE(reminder_->IsTorchReady());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_TorchReadyAuto_001
+ * @tc.name     test torch ready with mode auto
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, TorchReadyAuto, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(2);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_TRUE(reminder_->IsTorchReady());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_ScreenLocked_001
+ * @tc.name     test screen locked branch
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, ScreenLocked, TestSize.Level0)
+{
+    Mock_SetIsScreenLocked(true);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_TRUE(reminder_->IsScreenStatusSatisfied());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_ScreenNotLocked_001
+ * @tc.name     test screen not locked branch
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, ScreenNotLocked, TestSize.Level0)
+{
+    Mock_SetIsScreenLocked(false);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_FALSE(reminder_->IsScreenStatusSatisfied());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_Necessary_ScreenLocked_TorchReady_001
+ * @tc.name     test necessary condition satisfied
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, Necessary_ScreenLocked_TorchReady, TestSize.Level0)
+{
+    Mock_SetIsScreenLocked(true);
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_TRUE(reminder_->IsFlashRemindNecessary());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_NotNecessary_ScreenNotLocked_001
+ * @tc.name     test not necessary when screen not locked
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, NotNecessary_ScreenNotLocked, TestSize.Level0)
+{
+    Mock_SetIsScreenLocked(false);
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_FALSE(reminder_->IsFlashRemindNecessary());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_NotNecessary_TorchNotSupported_001
+ * @tc.name     test not necessary when torch not supported
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, NotNecessary_TorchNotSupported, TestSize.Level0)
+{
+    Mock_SetIsScreenLocked(true);
+    Mock_SetIsTorchSupported(false);
+    Mock_SetTorchMode(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_FALSE(reminder_->IsFlashRemindNecessary());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_NotNecessary_TorchAlreadyOn_001
+ * @tc.name     test not necessary when torch already on
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, NotNecessary_TorchAlreadyOn, TestSize.Level0)
+{
+    Mock_SetIsScreenLocked(true);
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(1);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    EXPECT_FALSE(reminder_->IsFlashRemindNecessary());
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_SwitchTorchFromOff_001
+ * @tc.name     test switch torch from off to on
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, SwitchTorchFromOff, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(0);
+    Mock_SetSetTorchModeResult(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    reminder_->HandleSetTorchMode();
+    EXPECT_EQ(Mock_GetTorchMode(), 1);
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_SwitchTorchFromOn_001
+ * @tc.name     test switch torch from on to off
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, SwitchTorchFromOn, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(1);
+    Mock_SetSetTorchModeResult(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    reminder_->HandleSetTorchMode();
+    EXPECT_EQ(Mock_GetTorchMode(), 0);
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_StopWhenUsed_001
+ * @tc.name     test stop flash remind when used
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, StopWhenUsed, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(1);
+    Mock_SetSetTorchModeResult(0);
+    Mock_SetFreeCameraResult(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    reminder_->isFlashRemindUsed_ = true;
+    callbackCalled_ = false;
+    reminder_->HandleStopFlashRemind();
+    EXPECT_EQ(Mock_GetTorchMode(), 0);
+    EXPECT_FALSE(reminder_->isFlashRemindUsed_);
+    EXPECT_TRUE(callbackCalled_);
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_StopWhenNotUsed_001
+ * @tc.name     test stop flash remind when not used
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, StopWhenNotUsed, TestSize.Level0)
+{
+    reminder_->libAdapterHandler_ = nullptr;
+    reminder_->isFlashRemindUsed_ = false;
+    callbackCalled_ = false;
+    reminder_->HandleStopFlashRemind();
+    EXPECT_TRUE(callbackCalled_);
+    EXPECT_FALSE(reminder_->isFlashRemindUsed_);
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_DestructorWhenUsed_001
+ * @tc.name     test destructor when flash remind used
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, DestructorWhenUsed, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(1);
+    Mock_SetSetTorchModeResult(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    reminder_->isFlashRemindUsed_ = true;
+    reminder_.reset();
+    reminder_ = nullptr;
+    EXPECT_EQ(Mock_GetTorchMode(), 0);
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_DestructorWhenNotUsed_001
+ * @tc.name     test destructor when flash remind not used
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, DestructorWhenNotUsed, TestSize.Level0)
+{
+    reminder_->libAdapterHandler_ = nullptr;
+    reminder_->isFlashRemindUsed_ = false;
+    reminder_.reset();
+    reminder_ = nullptr;
+    EXPECT_TRUE(true);
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_ProcessDelaySetTorchEvent_001
+ * @tc.name     test process delay set torch event
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, ProcessDelaySetTorchEvent, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(0);
+    Mock_SetSetTorchModeResult(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    auto event = AppExecFwk::InnerEvent::Get(DELAY_SET_TORCH_EVENT, 0);
+    reminder_->ProcessEvent(event);
+    EXPECT_EQ(Mock_GetTorchMode(), 1);
+    dlclose(reminder_->libAdapterHandler_);
+    reminder_->libAdapterHandler_ = nullptr;
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_ProcessStopFlashRemindEvent_001
+ * @tc.name     test process stop flash remind event
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, ProcessStopFlashRemindEvent, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(1);
+    Mock_SetSetTorchModeResult(0);
+    Mock_SetFreeCameraResult(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    reminder_->isFlashRemindUsed_ = true;
+    callbackCalled_ = false;
+    auto event = AppExecFwk::InnerEvent::Get(STOP_FLASH_REMIND_EVENT, 0);
+    reminder_->ProcessEvent(event);
+    EXPECT_TRUE(callbackCalled_);
+    EXPECT_FALSE(reminder_->isFlashRemindUsed_);
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_ProcessStartFlashRemindEvent_001
+ * @tc.name     test process start flash remind event
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, ProcessStartFlashRemindEvent, TestSize.Level0)
+{
+    Mock_SetIsScreenLocked(true);
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(0);
+    reminder_->isFlashRemindUsed_ = false;
+    auto event = AppExecFwk::InnerEvent::Get(START_FLASH_REMIND_EVENT, 0);
+    reminder_->ProcessEvent(event);
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_ProcessUnknownEvent_001
+ * @tc.name     test process unknown event
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, ProcessUnknownEvent, TestSize.Level0)
+{
+    auto event = AppExecFwk::InnerEvent::Get(99999, 0);
+    reminder_->ProcessEvent(event);
+    EXPECT_TRUE(true);
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_StartFlashRemind_Normal_001
+ * @tc.name     test start flash remind normal flow
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, StartFlashRemind_Normal, TestSize.Level0)
+{
+    Mock_SetIsScreenLocked(true);
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(0);
+    reminder_->isFlashRemindUsed_ = false;
+    reminder_->StartFlashRemind();
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_StartFlashRemind_AlreadyStarted_001
+ * @tc.name     test start flash remind already started
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, StartFlashRemind_AlreadyStarted, TestSize.Level0)
+{
+    reminder_->isFlashRemindUsed_ = true;
+    reminder_->StartFlashRemind();
+    EXPECT_TRUE(reminder_->isFlashRemindUsed_);
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_StopFlashRemind_Normal_001
+ * @tc.name     test stop flash remind normal flow
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, StopFlashRemind_Normal, TestSize.Level0)
+{
+    Mock_SetIsTorchSupported(true);
+    Mock_SetTorchMode(1);
+    Mock_SetSetTorchModeResult(0);
+    Mock_SetFreeCameraResult(0);
+    reminder_->libAdapterHandler_ = dlopen("libtel_cm_deps_adapter_test.z.so", RTLD_LAZY);
+    ASSERT_NE(reminder_->libAdapterHandler_, nullptr);
+    reminder_->isFlashRemindUsed_ = true;
+    callbackCalled_ = false;
+    reminder_->StopFlashRemind();
+}
+
+/**
+ * @tc.number   Telephony_IncomingFlashReminder_StopFlashRemind_NotUsed_001
+ * @tc.name     test stop flash remind not used
+ * @tc.desc     Function test
+ */
+HWTEST_F(IncomingFlashReminderTest, StopFlashRemind_NotUsed, TestSize.Level0)
+{
+    reminder_->isFlashRemindUsed_ = false;
+    callbackCalled_ = false;
+    reminder_->StopFlashRemind();
+    EXPECT_TRUE(callbackCalled_);
+}
+
 } // namespace Telephony
 } // namespace OHOS
