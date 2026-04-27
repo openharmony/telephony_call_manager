2efc660f8f1efe1600e759626e914a0515f68d76
wxic <wangxichun@huawei.com>
2026-04-27 19:34:53 +0800

    update: 更新文件 app_state_observer.cpp
    Signed-off-by: wxic <wangxichun@huawei.com>

--- services/call_report/src/app_state_observer.cpp
+++ services/call_report/src/app_state_observer.cpp
@@ -33,12 +33,17 @@ ApplicationStateObserver::~ApplicationStateObserver() {}
 
 void ApplicationStateObserver::OnProcessDied(const AppExecFwk::ProcessData& processData)
 {
+    TELEPHONY_LOGI("OnProcessDied enter, pid: %{public}d", processData.pid);
     if (processData.processName == "ui") {
         TELEPHONY_LOGE("report voipcall state idle");
         DelayedSingleton<CallControlManager>::GetInstance()->SetVoIPCallState((int32_t)CallStateToApp::CALL_STATE_IDLE);
         DelayedSingleton<CallObjectManager>::GetInstance()->ClearVoipList();
         DelayedSingleton<BluetoothCallManager>::GetInstance()->
             SendBtCallState(0, 0, (int32_t)TelCallState::CALL_STATUS_IDLE, "");
+    }
+    auto callControlManager = DelayedSingleton<CallControlManager>::GetInstance();
+    if (callControlManager != nullptr) {
+        callControlManager->UpdateCallUI(false, processData.pid);
     }
 }
 
