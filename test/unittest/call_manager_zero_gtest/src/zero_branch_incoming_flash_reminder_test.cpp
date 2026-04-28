b73c3ae1964ba6d41926faba68cf9c512503206b
gcw_8avsHzpw <hanjinghui2@huawei.com>
2026-04-28 09:51:28 +0800

    修复bug
    Signed-off-by: gcw_8avsHzpw <hanjinghui2@huawei.com>

--- test/unittest/call_manager_zero_gtest/src/zero_branch_incoming_flash_reminder_test.cpp
+++ test/unittest/call_manager_zero_gtest/src/zero_branch_incoming_flash_reminder_test.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #define private public
 #define protected public
 #include "incoming_flash_reminder.h"
