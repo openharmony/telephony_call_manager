cb89124b825701976a5f06cc96b544b4f16674f0
wxic <wangxichun@huawei.com>
2026-04-30 11:05:50 +0800

    update: 更新文件 call_manager_service.cpp
    Signed-off-by: wxic <wangxichun@huawei.com>

--- services/call_manager_service/src/call_manager_service.cpp
+++ services/call_manager_service/src/call_manager_service.cpp
@@ -1720,7 +1720,7 @@ int32_t CallManagerService::SendCallUiEvent(int32_t callId, std::string &eventNa
     return TELEPHONY_SUCCESS;
 }
 
-int32_t CallManagerService::UpdateCallUI(bool isConnectService)
+int32_t CallManagerService::IsPreloadCallUI(bool isConnectService)
 {
     if (!TelephonyPermission::CheckCallerIsSystemApp()) {
         TELEPHONY_LOGE("Non-system applications use system APIs!");
@@ -1732,7 +1732,7 @@ int32_t CallManagerService::UpdateCallUI(bool isConnectService)
     }
     if (callControlManagerPtr_ != nullptr) {
         int32_t callingPid = IPCSkeleton::GetCallingPid();
-        callControlManagerPtr_->UpdateCallUI(isConnectService, callingPid);
+        callControlManagerPtr_->IsPreloadCallUI(isConnectService, callingPid);
     } else {
         TELEPHONY_LOGE("callControlManagerPtr_ is nullptr!");
         return TELEPHONY_ERR_LOCAL_PTR_NULL;
