44fdeaa9570c03a741ede63d61d4efee9fe83958
wxic <wangxichun@huawei.com>
2026-04-30 17:04:22 +0800

    update: 更新文件 call_manager_service_proxy.h
    Signed-off-by: wxic <wangxichun@huawei.com>

--- frameworks/native/include/call_manager_service_proxy.h
+++ frameworks/native/include/call_manager_service_proxy.h
@@ -732,12 +732,12 @@ public:
     int32_t SendCallUiEvent(int32_t callId, std::string &eventName) override;
 
     /**
-     * IsPreloadCallUI
+     * PreloadCallUI
      *
      * @brief update callui
      * @return Returns 0 on success, others on failure.
      */
-    int32_t IsPreloadCallUI(bool isConnectService) override;
+    int32_t PreloadCallUI(bool isConnectService) override;
 
     /**
      * RegisterBluetoothCallManagerCallback
