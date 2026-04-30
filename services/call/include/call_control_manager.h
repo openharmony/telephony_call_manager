370b8c80f6eed0cafa41edece3e4fb1ac95f2f50
wxic <wangxichun@huawei.com>
2026-04-30 10:55:55 +0800

    update: 更新文件 call_control_manager.h
    Signed-off-by: wxic <wangxichun@huawei.com>

--- services/call/include/call_control_manager.h
+++ services/call/include/call_control_manager.h
@@ -175,7 +175,7 @@ public:
     void RegisterObserver();
     void UnRegisterObserver();
     void HandleVideoRingPlayFail();
-    void UpdateCallUI(bool isConnectService, int32_t callingPid);
+    void IsPreloadCallUI(bool isConnectService, int32_t callingPid);
     bool EndCall();
     int32_t SetCallAudioMode(int32_t mode, int32_t scenarios);
 #ifdef CALL_MANAGER_THERMAL_PROTECTION
