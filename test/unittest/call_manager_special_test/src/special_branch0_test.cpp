0bdf733208f0630c2bad6920033e6f9b73c1f29f
wxic <wangxichun@huawei.com>
2026-05-06 10:25:12 +0800

    update: 更新文件 special_branch0_test.cpp
    Signed-off-by: wxic <wangxichun@huawei.com>

--- test/unittest/call_manager_special_test/src/special_branch0_test.cpp
+++ test/unittest/call_manager_special_test/src/special_branch0_test.cpp
@@ -1028,7 +1028,7 @@ HWTEST_F(SpecialBranch0Test, Telephony_CallManagerService_019, TestSize.Level1)
     eventName = "123";
     int32_t ret = callManagerService->SendCallUiEvent(1, eventName);
     EXPECT_EQ(ret, 0);
-    ret = callManagerService->PreloadCallUI(true);
+    ret = callManagerService->PreloadCallUi(true);
     EXPECT_EQ(ret, 0);
 }
 
