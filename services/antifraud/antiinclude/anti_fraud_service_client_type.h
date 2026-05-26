85bf606f01fd571b0114f7406e97e92b5f4bb22d
lizhiqi_hw <lizhiqi14@huawei.com>
2026-05-26 19:10:02 +0800

    update: 更新文件 anti_fraud_service_client_type.h
    Signed-off-by: lizhiqi_hw <lizhiqi14@huawei.com>

--- services/antifraud/antiinclude/anti_fraud_service_client_type.h
+++ services/antifraud/antiinclude/anti_fraud_service_client_type.h
@@ -12,23 +12,170 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #ifndef ANTI_FRAUD_SERVICE_CLIENT_TYPE_H
 #define ANTI_FRAUD_SERVICE_CLIENT_TYPE_H
- 
+
 #include <string>
- 
+#include <cstdint>
+
+#include "parcel.h"
+#include "afs_detect_type.h"
+
 namespace OHOS::AntiFraudService {
- 
-typedef struct {
+enum VoiceType {
+    VOIP_CALL = 0,
+    VOICE_CALL = 1,
+    UNKNOWN = 2
+};
+
+enum ModelTypeBit {
+    VOICE_MODEL_BIT = 0b1,
+    PICTURE_MODEL_BIT = 0b10,
+    SPEECH_SYNTHESIS_MODEL_BIT = 0b100,
+    VOIP_CALL_TRANSFER_MODEL_BIT = 0b1000,
+    PUBLIC_SECURITY_MODEL_BIT = 0b10000,
+    WEB_ICON_MODEL_BIT = 0b100000,
+    WEB_SEMANTIC_MODEL_BIT = 0b1000000
+};
+
+struct AntiFraudResult {
     int errCode;
     bool result;
     int modelVersion;
     int fraudType;
-    std::string text;
+    std::string voiceText;
     float pvalue;
-} AntiFraudResult;
- 
+};
+
+struct AntiFraudResultExt {
+    /**
+     * Indicates the anti-fraud result of voice semantic detection.
+     * - false：no risk
+     * - true：risk
+     */
+    bool isVoiceSemanticFraud;
+    /**
+     * Indicates the anti-fraud result of speech synthesis detection.
+     * - false：no risk
+     * - true：risk
+     */
+    bool isSpeechSynthesisFraud;
+    /**
+     * Indicates the anti-fraud result of xoip call detection.
+     * - false：no risk
+     * - true：risk
+     */
+    float speechSynthesisProb;
+    /**
+     * Indicates the anti-fraud result of xoip call detection.
+     * - false：no risk
+     * - true：risk
+     */
+    bool isXoipFraud;
+};
+
+using VoiceDetectionResult = AntiFraudResult;
+
+struct SpeechSynthesisResult {
+    int errCode;
+    bool result;
+    int modelVersion;
+    float pvalue;
+};
+
+struct Rect {
+    uint32_t x;
+    uint32_t y;
+    uint32_t width;
+    uint32_t height;
+};
+
+struct VideoDetectResult {
+    int errCode;
+    bool result;
+    int modelVersion;
+    float pvalue;
+    Rect rect;
+    int imgFd;
+};
+
+struct VoipCallTransferResult {
+    int errCode;
+    bool result;
+    float pvalue;
+};
+
+struct PublicSecurityResult {
+    int errCode;
+    bool result;
+    int modelVersion;
+    float pvalue;
+    int fraudClass;
+};
+
+struct StartDetectionResult {
+    int errCode;
+    VoiceDetectionResult voiceDetectionResult;
+    SpeechSynthesisResult speechSynthesisResult;
+    VideoDetectResult videoDetectResult;
+    VoipCallTransferResult voipCallTransferResult;
+    PublicSecurityResult publicSecurityResult;
+};
+
+struct InitializeResult {
+    int errCode;
+};
+
+#ifdef DEBUG_MODE
+struct Filter {
+    double luminance;
+    int contrast;
+    double area;
+};
+#endif
+
+struct DetectionResult {
+    Rect rect;
+    float detectionProbability;
+    int modelVersion;
+    bool detectionResult;
+    int32_t result;
+#ifdef DEBUG_MODE
+    struct Filter info;
+#endif
+};
+
+struct WebAntiFraudResult {
+    int errCode;
+    bool isCheckSuccess;
+    int32_t hwCode;
+    std::string mappingType;
+    int32_t policy;
+    int32_t childModePolicy;
+    std::string url;
+    std::string context;
+    bool semanticIsFraud;
+    float semanticFraudProb;
+    int32_t semanticFraudType;
+    bool isNoticeXiaoyi;
+    std::string brandName;
+    std::string subClassName;
+    bool hasIcon;
+    std::vector<std::string> detectedIcons;
+    bool keywordSuccess;
+    std::vector<std::string> hitKeywords;
+};
+
+struct DownloadResult {
+    bool needNotify;
+    int size;
+    int status;
+    int process;
+};
+
+using ListenerResult = std::variant<AntiFraudResult, InitializeResult, DetectionResult, DownloadResult,
+    WebAntiFraudResult, StartDetectionResult, PublicSecurityResult>;
 }
- 
+
 #endif
