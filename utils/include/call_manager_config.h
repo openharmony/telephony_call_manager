aecdb0bd284e1553ae17575362a05b201aa3ae72
lizhiqi_hw <lizhiqi14@huawei.com>
2026-06-02 17:08:18 +0800

    new: 新建文件 call_manager_config.h
    Signed-off-by: lizhiqi_hw <lizhiqi14@huawei.com>

---
+++ utils/include/call_manager_config.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CALL_MANAGER_CONFIG_H
+#define CALL_MANAGER_CONFIG_H
+
+#include <string>
+#include <unordered_set>
+#include <cJSON.h>
+#include <atomic>
+#include <mutex>
+#include "ffrt.h"
+
+namespace OHOS {
+namespace Telephony {
+
+class CallManagerConfig {
+public:
+    static bool ShouldHangUpCellularCallBeforeAnswer(int32_t uid);
+    static bool ShouldConvertUsernameToPhoneNum(int32_t uid);
+
+private:
+    static void Init();
+    static int32_t ParserConfigJson();
+    static int32_t LoaderJsonFile(char *&content, const char *path);
+    static void CloseFile(FILE *f);
+    static std::string ParseString(const cJSON *value);
+    static void ParseFeatureList(const cJSON *root, const char *itemName, std::unordered_set<std::string> &featureList);
+    static int32_t ReadFileContent(FILE *f, char *&content, long len);
+    static std::string BuildMatchKey(int32_t uid, const char *methodName);
+
+    static std::unordered_set<std::string> hangUpCellularCallBeforeAnswerList_;
+    static std::unordered_set<std::string> convertUsernameToPhoneNumList_;
+    static std::atomic<bool> isInit_;
+    static ffrt::mutex mutex_;
+};
+
+} // namespace Telephony
+} // namespace OHOS
+#endif
