007c8eea67de3cb0860cb8aa438088fc4066fda7
niumingyu0 <niumingyu@huawei.com>
2026-06-27 11:32:04 +0800

    update: 更新文件 audio_control_manager.h
    Signed-off-by: niumingyu0 <niumingyu@huawei.com>

--- services/audio/include/audio_control_manager.h
+++ services/audio/include/audio_control_manager.h
@@ -138,7 +138,7 @@ private:
     void PostProcessRingtone();
     bool IsExternalAudioDevice(AudioDeviceType initDeviceType);
     bool IsInEarAudioDevice(AudioDeviceType initDeviceType);
-    void AdjustDeviceForNonNormalRingMode(AudioDevice &device, AudioDeviceType deviceType);
+    void AdjustDeviceForCrs(AudioDevice &device, AudioDeviceType deviceType);
     void UnmuteSoundTone();
     void ProcessSoundtone(sptr<CallBase> &callObjectPtr);
     void MuteWaitingTone();
