1e617241e28e87466b3991427d71304b5e37f961
niumingyu0 <niumingyu@huawei.com>
2026-09-14 20:40:24 +0800

    update: 更新文件 call_manager_service.cpp
    Signed-off-by: niumingyu0 <niumingyu@huawei.com>

--- services/call_manager_service/src/call_manager_service.cpp
+++ services/call_manager_service/src/call_manager_service.cpp
@@ -1960,7 +1960,6 @@ int32_t CallManagerService::RejectCall(RejectType rejectType)
         return TELEPHONY_ERR_ILLEGAL_USE_OF_SYSTEM_API;
     }
     if (!TelephonyPermission::CheckPermission(OHOS_PERMISSION_ANSWER_CALL) &&
-        !TelephonyPermission::CheckPermission(OHOS_PERMISSION_SET_TELEPHONY_STATE) &&
         !TelephonyPermission::CheckPermission(OHOS_PERMISSION_MANAGE_CALL_FOR_DEVICES)) {
         TELEPHONY_LOGE("Permission denied!");
         return TELEPHONY_ERR_PERMISSION_ERR;
